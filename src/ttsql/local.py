7784ef3c6a903ca57a2e27d45b38a652f2e4b785
Tugaytalha <mukoicen@gmail.com>
2025-03-12 12:20:49 +0300

    feat: integrate local context support for Ollama in local calls

--- src/ttsql/local.py
+++ src/ttsql/local.py
@@ -1,8 +1,17 @@
 from .chromadb.chromadb_vector import ChromaDB_VectorStore
 from .openai.openai_chat import OpenAI_Chat
+from .ollama.ollama import Ollama
 
 
 class LocalContext_OpenAI(ChromaDB_VectorStore, OpenAI_Chat):
     def __init__(self, config=None):
         ChromaDB_VectorStore.__init__(self, config=config)
         OpenAI_Chat.__init__(self, config=config)
+
+
+class LocalContext_Ollama(ChromaDB_VectorStore, Ollama):
+    def __init__(self, config=None):
+        ChromaDB_VectorStore.__init__(self, config=config)
+        Ollama.__init__(self, config=config)
+
+        self._model = config["model"]
